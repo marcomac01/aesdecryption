e2958b89850d271896ba86c0599d899fdbc8007c
Marco Macrì <macrimarco001@gmail.com>
2022-04-08 15:24:10 +0200

    Esercizio completato. Manca documentazione.

--- src/Test.java
+++ src/Test.java
@@ -3,23 +3,5 @@ import java.io.File;
 public class Test {
     public static void main(String[] args) throws Exception {
         Metodi.bruteForce(4, new File("document2022.encrypted"), "SISOP-corsoB");
-        //Metodi.bruteForce(4,new File("document.encrypted"), "uffi");
-        //document.decryptedOK
-        /*byte[] chiave = Metodi.dammiChiave(1301770212);
-        byte[] indizio = "SISOP-corsoB".getBytes();
-        byte[] input = Metodi.leggiFile(new File("document2022.encrypted"));
-
-        try {
-
-            byte[] arr = Metodi.decifra("0000001301770212".getBytes(), input);
-            File fine = new File("fine.dec");
-            FileOutputStream outputStream = new FileOutputStream(fine);
-            outputStream.write(arr);
-            outputStream.close();
-        } catch (Exception e) {
-
-        }
-
-*/
     }
 }
