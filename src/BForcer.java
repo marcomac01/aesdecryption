c1629343c5cf068d7a0e2a1f246273f01b283e47
marcomac01 <macrimarco001@gmail.com>
2023-10-23 17:02:29 +0200

    Cleanup

--- src/BForcer.java
+++ src/BForcer.java
@@ -1,3 +1,9 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.Key;
+
 class BForcer extends Thread{
     private final int inizio, fine, id;
     private BForcer[] bForces;
@@ -19,16 +25,16 @@ class BForcer extends Thread{
         System.out.println("Il thread "+ id + " comincia da "+inizio+" a "+fine);
         for (int i = inizio; i < fine && !this.isInterrupted(); i++) {
             try {
-                byte[] chiave = Metodi.dammiChiave(i);
-                outputArray = Metodi.decifra(chiave, inputArray);
-                if (Metodi.contenutoValido(outputArray, indizioArray)) {
-                    Metodi.scriviFile("Decifrato.dec", outputArray);
-                    System.out.println("La chiave trovata sembra essere "+ Metodi.dammiStringa(i));
+                byte[] chiave = dammiChiave(i);
+                outputArray = decifra(chiave, inputArray);
+                if (contenutoValido(outputArray, indizioArray)) {
+                    scriviFile("Decifrato.dec", outputArray);
+                    System.out.println("La chiave trovata sembra essere "+ dammiStringa(i));
                     System.out.println("Il file in output ha nome Decifrato.dec");
                     interrompiTuttiNotThis();
                     break;
-                }
-            } catch (Exception e) {
+                    }
+                } catch (Exception e) {
             }
         }
     }
@@ -36,4 +42,47 @@ class BForcer extends Thread{
     private void interrompiTuttiNotThis() {
         for(int i = 0; i < bForces.length; i++) if(i != this.id) bForces[i].interrupt();
     }
+
+    static String dammiStringa(Integer intero) {
+        if (intero < 0) throw new IllegalArgumentException("Valore negativo");
+        StringBuilder stringa = new StringBuilder();
+        for(int i = 0; i < 16 - intero.toString().length(); i++, stringa.append("0"));
+        stringa.append(intero);
+        return stringa.toString();
+    }
+    static byte[] dammiChiave(Integer intero) {
+        return dammiStringa(intero).getBytes();
+    }
+    static boolean contenutoValido(byte[] daVerificare, byte[] obiettivo) {
+        if (daVerificare.length < obiettivo.length) throw new IllegalArgumentException("Arrays in input non validi o invertiti");
+        int indiceObiettivo = 0;
+        for (int i = 0; i < daVerificare.length; i++) {
+            if (daVerificare[i] != obiettivo[indiceObiettivo]) {
+                indiceObiettivo = 0;
+            } else {
+                indiceObiettivo++;
+            }
+            if (indiceObiettivo == obiettivo.length) return true;
+        }
+        return false;
+    }
+
+    static void scriviFile(String pathNome, byte[] outputArray) throws Exception {
+        File file = new File(pathNome);
+        FileOutputStream outputStream = new FileOutputStream(file);
+        outputStream.write(outputArray);
+        outputStream.close();
+    }
+
+    static byte[] decifra(byte[] chiave, byte[] input) {
+        try {
+            Key chiaveSegreta = new SecretKeySpec(chiave, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, chiaveSegreta);
+            return cipher.doFinal(input);
+            } catch (Exception e) {
+        }
+        return null;
+    }
+
 }
--- src/Metodi.java
+++ src/Metodi.java
@@ -1,25 +1,13 @@
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.Key;
 import java.time.Duration;
 import java.time.Instant;
 
 public class Metodi {
-    static String dammiStringa(Integer intero) {
-        if (intero < 0) throw new IllegalArgumentException("Valore negativo");
-        StringBuilder stringa = new StringBuilder();
-        for(int i = 0; i < 16 - intero.toString().length(); i++, stringa.append("0"));
-        stringa.append(intero);
-        return stringa.toString();
-    }
 
-    static byte[] dammiChiave(Integer intero) {
-        return dammiStringa(intero).getBytes();
-    }
+
+
 
     public static void bruteForce(int nThread, File input, String indizio) throws Exception{
         Instant start = Instant.now();
@@ -41,19 +29,7 @@ public class Metodi {
         System.out.println("Tempo impiegato: " + Duration.between(start, end).toMinutes() + " minuti");
     }
 
-    static boolean contenutoValido(byte[] daVerificare, byte[] obiettivo) {
-        if (daVerificare.length < obiettivo.length) throw new IllegalArgumentException("Arrays in input non validi o invertiti");
-        int indiceObiettivo = 0;
-        for (int i = 0; i < daVerificare.length; i++) {
-            if (daVerificare[i] != obiettivo[indiceObiettivo]) {
-                indiceObiettivo = 0;
-            } else {
-                indiceObiettivo++;
-            }
-            if (indiceObiettivo == obiettivo.length) return true;
-        }
-        return false;
-    }
+
 
     static byte[] leggiFile(File input) throws IOException {
         FileInputStream fIS = new FileInputStream(input);
@@ -63,22 +39,8 @@ public class Metodi {
         return ret;
     }
 
-    static void scriviFile(String pathNome, byte[] outputArray) throws Exception {
-        File file = new File(pathNome);
-        FileOutputStream outputStream = new FileOutputStream(file);
-        outputStream.write(outputArray);
-        outputStream.close();
-    }
 
-    static byte[] decifra(byte[] chiave, byte[] input) {
-        try {
-            Key chiaveSegreta = new SecretKeySpec(chiave, "AES");
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, chiaveSegreta);
-            return cipher.doFinal(input);
-        } catch (Exception e) {
-        }
-        return null;
-    }
+
+
 
 }
